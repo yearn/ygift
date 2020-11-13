6b4209e63f8a511d9467ad2eed09dec91cc33ea4
banteg <banteeg@gmail.com>
2020-11-13 17:44:54 +0300

    feat: pin openzeppelin version

--- contracts/yGift/yGift.sol
+++ contracts/yGift/yGift.sol
@@ -1,42 +1,10 @@
 // SPDX-License-Identifier: MIT
+pragma solidity =0.6.12;
 
-pragma solidity ^0.6.4;
-
-import "../erc721/ERC721.sol";
-import "../erc20/IERC20.sol";
-import "../utils/SafeMath.sol";
-
-library SafeERC20 {
-	using SafeMath for uint256;
-	using Address for address;
-
-	function safeTransfer(IERC20 token, address to, uint256 value) internal {
-		callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
-	}
-
-	function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-		callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
-	}
-
-	function safeApprove(IERC20 token, address spender, uint256 value) internal {
-		require((value == 0) || (token.allowance(address(this), spender) == 0),
-			"SafeERC20: approve from non-zero to non-zero allowance"
-		);
-		callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
-	}
-	function callOptionalReturn(IERC20 token, bytes memory data) private {
-		require(address(token).isContract(), "SafeERC20: call to non-contract");
-
-		// solhint-disable-next-line avoid-low-level-calls
-		(bool success, bytes memory returndata) = address(token).call(data);
-		require(success, "SafeERC20: low-level call failed");
-
-		if (returndata.length > 0) { // Return data is optional
-			// solhint-disable-next-line max-line-length
-			require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
-		}
-	}
-}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract yGift is ERC721("yearn Gift NFT", "yGIFT") {
 	using SafeERC20 for IERC20;
